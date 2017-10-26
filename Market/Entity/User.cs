5993f3603a505c2760707b3e29f8239f5bdac8d4
centraldemon <kurilovich1999@gmail.com>
2017-10-26 17:46:18 +0300

    Anton Change

---
+++ Market/Entity/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Entity
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required,StringLength(maximumLength:225)]
+        public string  Login { get; set; }
+        [Required,StringLength(maximumLength:225)]
+        public string Password { get; set; }
+
+    }
+}
