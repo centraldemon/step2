f89b44833227dd81ef5cccdbfe5c64ecb6232f9e
ШАГ Академія <cuanid236316@gmail.com>
2017-10-26 17:41:00 +0300

    Create class Category and Product

---
+++ Market/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market
+{
+    public class Category
+    {
+        //[Key]
+        public int Id { get; set; }
+        public int Name { get; set; }
+        //public virtual Product categoty { get; set; }
+    }
+}
