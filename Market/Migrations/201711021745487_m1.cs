7e046eaaf532060001f094646c54eeb28e5689d8
centraldemon <kurilovich1999@gmail.com>
2017-11-02 19:46:38 +0200

    testcommit

---
+++ Market/Migrations/201711021745487_m1.cs
@@ -0,0 +1,27 @@
+namespace Market.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class m1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Login = c.String(nullable: false, maxLength: 225),
+                        Password = c.String(nullable: false, maxLength: 225),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
